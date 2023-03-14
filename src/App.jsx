6078a8534f7baa736aed92a1d425393622123a7c
Franco Buceta <francobuceta95@gmail.com>
2023-03-14 13:57:28 -0300

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1 className='text-3xl font-bold underline'>Custom Boutique</h1>
-      </div>
+      <Navbar />
     </>
   )
 }
