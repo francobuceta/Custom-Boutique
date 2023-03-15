ac98c382289ffda51fbdbee16f187e27c7908c5b
Franco Buceta <105551748+francobuceta@users.noreply.github.com>
2023-03-15 19:01:45 -0300

    Carousel (#6)
    * add carousel component
    
    * add carouselContainer and react-scroll

---
+++ src/pages/Home.jsx
@@ -0,0 +1,95 @@
+import Carousel from "../components/Carousel";
+import slide1 from "../assets/carousel/slide1.png";
+import slide2 from "../assets/carousel/slide2.png";
+import slide3 from "../assets/carousel/slide3.png";
+import slide4 from "../assets/carousel/slide4.png";
+import logo from "../assets/logo.png";
+
+const Home = () => {
+
+    const slides1 = [
+        {
+            url: slide1
+        },
+        {
+            url: logo
+        },
+        {
+            url: slide1
+        },
+        {
+            url: logo
+        }
+    ];
+
+    const slides2 = [
+        {
+            url: slide2
+        },
+        {
+            url: logo
+        },
+        {
+            url: slide2
+        },
+        {
+            url: logo
+        }
+    ];
+
+    const slides3 = [
+        {
+            url: slide3
+        },
+        {
+            url: logo
+        },
+        {
+            url: slide3
+        },
+        {
+            url: logo
+        }
+    ];
+
+    const slides4 = [
+        {
+            url: slide4
+        },
+        {
+            url: logo
+        },
+        {
+            url: slide4
+        },
+        {
+            url: logo
+        }
+    ];
+
+    return (
+        <>
+
+            <section className="grid gap-2 py-10 px-4"
+                style={{ gridTemplateColumns: "2fr 1fr 1fr 0fr" }} id="home" > 
+
+                <div>
+                    <Carousel width="680px" height="576px" array={slides1} />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <Carousel width="317px" height="285px" array={slides2} />
+                    <Carousel width="317px" height="285px" array={slides3} />
+                </div>
+
+                <div>
+                    <Carousel width="330px" height="576px" array={slides4} />
+                </div>
+
+            </section>
+
+        </>
+    )
+}
+
+export default Home;
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,14 +1,15 @@
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Link } from "react-scroll";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
 
     const navigation = [
-        { name: 'Inicio', href: '#', current: true },
-        { name: 'Instagram', href: '#', current: false },
-        { name: 'Promociones', href: '#', current: false },
-        { name: 'Contacto', href: '#', current: false },
+        { name: 'Inicio', href: 'home' },
+        { name: 'Instagram', href: '#' },
+        { name: 'Promociones', href: '#' },
+        { name: 'Contacto', href: '#' },
     ]
 
     function classNames(...classes) {
@@ -16,7 +17,7 @@ const Navbar = () => {
     }
 
     return (
-        <Disclosure as="nav" className="bg-slate-100">
+        <Disclosure as="nav" className="bg-slate-100 sticky top-0 z-50">
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -48,17 +49,18 @@ const Navbar = () => {
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <a
+                                            <Link
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current ? 'navbar_textCurrent text-2xl' : 'text-black navbar_text text-xl',
-                                                    'rounded-md px-3 lg:px-10 py-2 font-medium'
+                                                    'text-black navbar_text text-xl rounded-md px-3 lg:px-10 py-2 font-medium'
                                                 )}
                                                 aria-current={item.current ? 'page' : undefined}
+                                                activeClass="navbar_textCurrent" smooth spy to={item.href} 
+                                                offset={item.href === "home" ? -500 : -100}
                                             >
                                                 {item.name}
-                                            </a>
+                                            </Link>
                                         ))}
                                     </div>
                                 </div>
