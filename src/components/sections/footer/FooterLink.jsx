7886d6f09562bc977b5a175615314927bb0de373
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-03-17 21:19:44 -0300

    fix: fixed footer link styles

--- src/components/sections/footer/FooterLink.jsx
+++ src/components/sections/footer/FooterLink.jsx
@@ -1,7 +1,7 @@
 const FooterLink = ({ href, img }) => (
-  <a className="flex flex-col md:flex-row items-center gap-[7px] text-white hover:text-primary transition-colors duration-300" href={href}>
+  <a className="flex flex-col md:flex-row items-center gap-[7px] md:gap-[10px] text-white" href={href}>
     <img src={img} alt="instagram logo" className="w-[27px] h-[27px]" />
-    <p className="text-[11.25px] md:text-[15px] leading-[13px] md:leading-[18px]">custombouti</p>
+    <p className="text-[11.25px] md:text-[16px] underline leading-[13px] md:leading-[19px]">custombouti</p>
   </a>
 );
 
