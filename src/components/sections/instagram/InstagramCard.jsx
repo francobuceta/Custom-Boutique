7b262f7f962d8600044d1bb68fb5ed0e9603973d
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-03-18 14:42:19 -0300

    fix: fixed instagram logo on card

--- src/components/sections/instagram/InstagramCard.jsx
+++ src/components/sections/instagram/InstagramCard.jsx
@@ -2,7 +2,9 @@ import { AiOutlineInstagram } from 'react-icons/ai';
 
 const InstagramCard = ({ src }) => (
   <article className="h-[165.63px] md:h-[327.17px] xl:h-[359px] bg-[#D9D9D9] rounded-[4.30px] col-span-6 xl:col-span-4 relative">
-    <AiOutlineInstagram className="absolute text-[20.5px] md:text-[34.65px] xl:text-[38px] bottom-[11.65px] md:bottom-[23.01px] xl:bottom-[25.25px] right-[8.6px] md:right-[16.99px] xl:right-[18.99px]" />
+    <div className="absolute bottom-[9.18px] md:bottom-[19px] xl:bottom-[22px] right-[6.1px] md:right-[13px] xl:right-4 rounded-full bg-[rgba(0,0,0,0.5)] p-1">
+      <AiOutlineInstagram className="text-[20.5px] md:text-[34.65px] xl:text-[38px] text-white" />
+    </div>
     <img src="https://humanidades.com/wp-content/uploads/2018/08/bosque-1-e1577589217640.jpg" alt="Instagram Post" className="rounded-[4.30px] w-full h-full" />
   </article>
 );
