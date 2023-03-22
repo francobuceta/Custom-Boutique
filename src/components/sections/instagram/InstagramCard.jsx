102c7b859e313e2eb80ee784f1c4e305ae46a218
Franco Buceta <francobuceta95@gmail.com>
2023-03-21 22:31:33 -0300

    fix z-index in instagram card

--- src/components/sections/instagram/InstagramCard.jsx
+++ src/components/sections/instagram/InstagramCard.jsx
@@ -2,7 +2,7 @@ import { AiOutlineInstagram } from 'react-icons/ai';
 
 const InstagramCard = ({ src, link }) => (
   <a href={link} target="_blank" className="cursor-pointer h-[165.63px] md:h-[327.17px] xl:h-[359px] bg-[#D9D9D9] rounded-[4.30px] col-span-6 xl:col-span-4 relative" rel="noreferrer">
-    <div className="absolute bottom-[9.18px] md:bottom-[19px] xl:bottom-[22px] right-[6.1px] md:right-[13px] xl:right-4 rounded-full bg-[rgba(0,0,0,0.5)] p-1 z-[50]">
+    <div className="absolute bottom-[9.18px] md:bottom-[19px] xl:bottom-[22px] right-[6.1px] md:right-[13px] xl:right-4 rounded-full bg-[rgba(0,0,0,0.5)] p-1 z-[40]">
       <AiOutlineInstagram className="text-[20.5px] md:text-[34.65px] xl:text-[38px] text-white" />
     </div>
     <img
